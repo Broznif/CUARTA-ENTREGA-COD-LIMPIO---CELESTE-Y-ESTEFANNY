5e052f79b2ec6c65ad39d7923e666487ae250475
Labvive <maria-celeste013@hotmail.com>
2024-05-15 23:52:51 -0500

    segundoool

--- SecretConfig.py
+++ SecretConfig.py
@@ -2,4 +2,4 @@ PGDATABASE = "entrega_owner"
 PGUSER = "entrega_owner_owner"
 PGPASSWORD = "Rofg6C8BcTVk"
 PGHOST = "ep-muddy-band-a581f951.us-east-2.aws.neon.tech"
-PGPORT = 5432 # POR DEFECTO ES 5432, PERO PUEDE CAMBIAR EN SU DB
+PGPORT = 5432
