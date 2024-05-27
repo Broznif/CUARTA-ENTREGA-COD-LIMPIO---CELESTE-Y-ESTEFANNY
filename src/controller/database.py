1bcf4c667b38197a370e30304c59bd37337b6d5e
Labvive <maria-celeste013@hotmail.com>
2024-05-27 11:39:07 -0500

    ahora siss

--- src/controller/database.py
+++ src/controller/database.py
@@ -11,8 +11,11 @@ class Database:
         try:
             self.connection = psycopg2.connect(self.connection_string)
             self.cursor = self.connection.cursor()
+            print("Conexión exitosa a la base de datos")
         except psycopg2.Error as e:
             print(f"Error al conectar a la base de datos: {e}")
+            self.connection = None
+            self.cursor = None
 
     def close(self):
         if self.cursor:
@@ -21,6 +24,10 @@ class Database:
             self.connection.close()
 
     def create_tables(self):
+        if not self.cursor:
+            print("No se puede crear la tabla porque el cursor es None. Asegúrate de que la conexión esté establecida.")
+            return
+        
         create_table_query = """
             CREATE TABLE IF NOT EXISTS my_schema.clustering_results (
                 id SERIAL PRIMARY KEY,
@@ -29,13 +36,25 @@ class Database:
                 cluster_results JSONB NOT NULL
             );
         """
-        self.cursor.execute(create_table_query)
-        self.connection.commit()
+        try:
+            self.cursor.execute(create_table_query)
+            self.connection.commit()
+            print("Tabla creada exitosamente")
+        except psycopg2.Error as e:
+            print(f"Error al crear la tabla: {e}")
 
     def insert_clustering_result(self, filename, num_clusters, cluster_results):
+        if not self.cursor:
+            print("No se puede insertar datos porque el cursor es None. Asegúrate de que la conexión esté establecida.")
+            return
+        
         insert_query = """
             INSERT INTO my_schema.clustering_results (filename, num_clusters, cluster_results)
             VALUES (%s, %s, %s);
         """
-        self.cursor.execute(insert_query, (filename, num_clusters, json.dumps(cluster_results)))
-        self.connection.commit()
+        try:
+            self.cursor.execute(insert_query, (filename, num_clusters, json.dumps(cluster_results)))
+            self.connection.commit()
+            print("Datos insertados exitosamente")
+        except psycopg2.Error as e:
+            print(f"Error al insertar datos: {e}")
